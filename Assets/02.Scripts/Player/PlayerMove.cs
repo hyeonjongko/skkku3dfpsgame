0716d7c95ee84728ff23d764cde42dd884913d44
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2025-12-08 16:49:37 +0900

    feat 플레이어 움직임 및 카메라 회전 구현

---
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+// 키보드를 누르면 캐릭터를 그 방향으로 이동 시키고 싶다.
+public class PlayerMove : MonoBehaviour
+{
+    // 필요 속성
+    // - 이동속도
+    public float MoveSpeed = 7;
+    //-중력
+    public float Gravity = -9.81f;
+    private float _yVelocity = 0f;//중력에 의해 누적될 y값 변수
+
+    //-점프력
+    private float _jumpPower = 5f;
+
+    private CharacterController _controller;
+
+    private void Awake()
+    {
+     _controller = GetComponent<CharacterController>();   
+    }
+
+    private void Update()
+    {
+        //0. 중력을 누적한다.
+        _yVelocity += Gravity * Time.deltaTime;
+
+        // 1. 키보드 입력 받기
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        // 2. 입력에 따른 방향 구하기 
+        // 현재는 유니티 세상의 절대적인 방향이 기준 (글로벌/월드 좌표계)
+        // 내가 원하는 것은 카메라가 쳐다보는 방향이 기준으로
+
+        // - 글로벌 좌표 방향을 구한다. 
+        Vector3 direction = new Vector3(x, 0, y);
+        direction.Normalize();
+
+        //Debug.Log(_controller.collisionFlags);
+
+        //- 점프 : 점프키를 눌렀을 때 땅이라면
+        if (Input.GetButtonDown("Jump") && _controller.isGrounded)
+        {
+            _yVelocity = _jumpPower;
+        }
+
+
+        // - 카메라가 쳐다보는 방향으로 변환한다. (월드 -> 로컬)
+        direction = Camera.main.transform.TransformDirection(direction);
+        direction.y = _yVelocity; //중력 적용(좌표를 월드에서 로컬로 바꿔준 이후에 해주어야 한다)
+
+        // 3. 방향으로 이동시키기  
+        _controller.Move(direction * MoveSpeed * Time.deltaTime);
+    }
+
+}
