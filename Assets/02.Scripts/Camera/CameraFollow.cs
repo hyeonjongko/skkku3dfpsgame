3cd8f2b8bbfd5a680cd8dd0a39d8c1e1ef26b073
hyeonjongko <115774307+hyeonjongko@users.noreply.github.com>
2025-12-09 18:24:23 +0900

    feat 과제 3번 카메라 시점 변환 구현

--- Assets/02.Scripts/Camera/CameraFollow.cs
+++ Assets/02.Scripts/Camera/CameraFollow.cs
@@ -1,13 +1,35 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 // 목표를 따라다니는 카메라
 public class CameraFollow : MonoBehaviour
 {
-    public Transform Target;
+    public Transform FPSTarget;
+    public Transform TPSTarget;
 
+    private bool _isFPS = true;
+
+    private void Start()
+    {
+        transform.position = FPSTarget.position;
+        transform.rotation = FPSTarget.rotation;
+    }
     private void LateUpdate()
     {
-        transform.position = Target.position;
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _isFPS = !_isFPS;
+
+        }
+        if (_isFPS)
+        {
+            transform.position = FPSTarget.position;
+            transform.rotation = FPSTarget.rotation;
+        }
+        else
+        {
+            transform.rotation = TPSTarget.rotation;
+            transform.position = TPSTarget.position;
+        }
     }
 
 }
